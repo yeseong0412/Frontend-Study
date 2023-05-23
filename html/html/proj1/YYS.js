fac3e18348133cd7d10f56f06583b8dbec4225d7
양예성 <127856153+yeseong0412@users.noreply.github.com>
2023-05-23 19:35:09 +0900

    Add files via upload

---
+++ html/html/proj1/YYS.js
@@ -0,0 +1,13 @@
+function m0k(){
+    document.getElementById("idH5").innerHTML="메롱!!";
+}
+function mshow(pGbn){
+    if(pGbn==2)
+        document.getElementById("idH5").style.display="none";
+    else if(pGbn==1)
+        document.getElementById("idH5").style.display="block";
+    else if(pGbn==3)     
+        document.getElementById("idImg").src="http://arabbit.link/images/tiger.png"
+    else if(pGbn==4)
+        document.getElementById("idcolor").style="color:green"
+}
